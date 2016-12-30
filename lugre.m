1f5ed2e2cdf2f81d6f716b64be7fa4f2ef3d4d8f
Auralius Manurung <manurung.auralius@gmail.com>
2016-12-30 08:35:32 +0100

    Added a plot to show the transient behaviour at one certain velocity.

--- lugre.m
+++ lugre.m
@@ -1,4 +1,4 @@
-%% 
+%%
 clear all;
 close all;
 clc;
@@ -16,7 +16,7 @@ ts = 1e-6;
 time_span = 0.1;
 t = 0 : ts : time_span;
 
-%%
+%% Velocity range
 v = -0.005:0.0001:0.005;
 
 %%
@@ -27,7 +27,7 @@ for i = 1 : length(v)
         g = (Fc + (Fs - Fc) * exp(r)) / sigma_0;
         z_dot = v(i) - abs(v(i)) * z / g;
         z = z + z_dot * ts;
-
+        
         F(j) = sigma_0 * z + sigma_1 * z_dot + sigma_2 * v(i);
     end
     Fss(i) = F(end);
@@ -38,3 +38,22 @@ grid
 xlabel('Velocity (m/s)')
 ylabel('Friction force (N)')
 title('Friction force at steady state condition')
+
+%% Zoom into certain velocity to see its transient behaviour
+clear F v;
+v = 0.002;
+for j = 1 : length(t)
+    r = -(v/vs)^2;
+    g = (Fc + (Fs - Fc) * exp(r)) / sigma_0;
+    z_dot = v - abs(v) * z / g;
+    z = z + z_dot * ts;
+    
+    F(j) = sigma_0 * z + sigma_1 * z_dot + sigma_2 * v;
+end
+
+figure
+plot(t, F);
+grid
+xlabel('Time (s)')
+ylabel('Friction force (N)')
+title('Friction force for v = 0.002')
