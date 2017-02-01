1c895c265caf851724bcae2ba1d67229d22bad6f
Auralius Manurung <manurung.auralius@gmail.com>
2017-02-01 13:34:37 +0100

    Hysteresis in friction with varying velocity.

--- lugre.m
+++ lugre.m
@@ -3,7 +3,7 @@ clear all;
 close all;
 clc;
 
-%%
+%% See Table I from the paper
 sigma_0 = 1e5;
 sigma_1  = sqrt(1e5);
 sigma_2  = 0.4;
@@ -42,6 +42,7 @@ title('Friction force at steady state condition')
 %% Zoom into certain velocity to see its transient behaviour
 clear F v;
 v = 0.002;
+z = 0;
 for j = 1 : length(t)
     r = -(v/vs)^2;
     g = (Fc + (Fs - Fc) * exp(r)) / sigma_0;
@@ -57,3 +58,31 @@ grid
 xlabel('Time (s)')
 ylabel('Friction force (N)')
 title('Friction force for v = 0.002')
+
+%% Apply sinusoidal velocity and measure the friction force (Fig. 3 of the paper)
+figure
+hold on
+color = ['rgb'];
+clear F v;
+t = 0 : ts : 10;
+omega = [1 10 25];
+for i = 1 : length(omega)
+    z = 0;
+    v = 0.001 * (sin(omega(i)*t)+1.5);
+    for j = 1 : length(t)
+        r = -(v(j)/vs)^2;
+        g = (Fc + (Fs - Fc) * exp(r)) / sigma_0;
+        z_dot = v(j) - abs(v(j)) * z / g;
+        z = z + z_dot * ts;
+
+        F(j) = sigma_0 * z + sigma_1 * z_dot + sigma_2 * v(i);
+    end
+    % Start from t = 3 up to the end
+    plot(v(3*1/ts:end), F(3*1/ts:end), color(i));
+end
+
+grid
+xlabel('Velocity (m/s)')
+ylabel('Friction force (N)')
+title('Hysteresis in friction with varying velocity')
+legend('1 rad/s', '10 rad/s', '25 rad/s')
